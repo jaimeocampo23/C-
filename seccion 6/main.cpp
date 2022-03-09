fa47c165a26f3a3c6e7de4fb7f5e93957fea6f21
jaimeocampo23 <jaimecalderon907@gmail.com>
2022-03-08 20:53:31 -0600

    Initial Commit

---
+++ seccion 6/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  seccion 6
+//
+//  Created by Ivana Castellanos Barrera on 08/03/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
