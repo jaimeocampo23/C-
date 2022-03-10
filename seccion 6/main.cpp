2e1c9e2e82447931ba797a80e117de5d53f9b025
jaimeocampo23 <jaimecalderon907@gmail.com>
2022-03-10 08:42:30 -0600

    Update main.cpp
    Seccion 6 C++ course

--- seccion 6/main.cpp
+++ seccion 6/main.cpp
@@ -7,8 +7,32 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main()
+{
+
+    cout << "Hello, welcome to Jaime's Carpet Cleaning Service" << endl;
+    cout << "\nHow many rooms would you like cleaned? ";
+
+    int number_of_rooms{0};
+    const int days{30};
+    cin >> number_of_rooms;
+
+    cout << "\nEstimate for carpete cleaning services" << endl;
+    cout << "Number of rooms: " << number_of_rooms << endl;
+    
+    const int price{30};
+    cout << "Price per room $ " << price << endl;
+    
+    int cost{price * number_of_rooms};
+    cout << "Cost: $ " << cost << endl;
+    
+    const float tax{0.06};
+    cout << "Tax: $ " << cost * tax << endl;
+    cout << "=====================================" << endl;
+    cout << "Total stimate: $ " << (cost) + (cost * tax) << endl;
+    cout << "This stimate is valid for " << days << " days" << endl;
+
     return 0;
 }
